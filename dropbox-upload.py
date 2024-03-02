64364ea7581435c1d25a453e55a264571851b040
T-Visor <turkimbro@yahoo.com>
2024-03-02 14:49:53 -0500

    first commit

---
+++ dropbox-upload.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+
+import dropbox
+import os
+import sys
+import re
+
+
+def main():
+    """
+        Precondition:
+            Access token created on Dropbox account with permission for 'files.content.write' enabled.
+
+        Description: 
+            Upload a local file to Dropbox.
+
+        Usage: 
+            python3 dropbox-upload.py <local_file_path> <dropbox_file_path> <access_token>
+    """
+    # Check if the correct number of command-line arguments are provided
+    if len(sys.argv) != 4:
+        print('Usage: python dropbox-upload.py <local_file_path> <dropbox_file_path> <access_token>')
+        return
+
+    # Parse command-line arguments
+    local_file_path = sys.argv[1]
+    dropbox_file_path = sys.argv[2]
+    access_token = sys.argv[3]
+
+    # Check if the local file exists
+    if not does_local_file_exist(local_file_path):
+        print(f'Local file "{local_file_path}" does not exist.')
+        return
+
+    # Check if supplied Dropbox file path is valid
+    if not is_valid_unix_file_path(dropbox_file_path):
+        print(f'Dropbox file_path "{dropbox_file_path}" is not a valid pathname')
+        return
+
+    # Proceed to upload to Dropbox
+    print(f'SOURCE: {local_file_path}')
+    print(f'DESTINATION: {dropbox_file_path}\n')
+    upload_to_dropbox(local_file_path, dropbox_file_path, access_token)
+
+
+def does_local_file_exist(file_path: str) -> bool:
+    """
+        Arguments:
+            file_path (str): local file path
+
+        Returns: 
+            'True' if the file exists
+            'False' otherwise
+    """
+    return is_valid_unix_file_path(file_path) and os.path.exists(file_path)
+
+
+def is_valid_unix_file_path(file_path: str) -> bool:
+    """
+        Arguments:
+            file_path (str): file path (local or remote machine)
+
+        Returns: 
+            'True' if the file path is a valid Unix file path
+            'False' otherwise
+    """
+    # Checks to see if the supplied string has the following characteristics:
+    # 
+    # Starts with a forward slash '/'
+    # One or more occurrences of a word
+    # Optionally contains an ending forward slash '/'
+    pattern = r'^\/(?:[\w.-]+\/?)+$'
+    return re.match(pattern, file_path) is not None
+
+
+def upload_to_dropbox(local_file_path: str, dropbox_file_path: str, access_token: str):
+    """
+        Description:
+            Upload the file to the location on Dropbox.
+
+        Arguments:
+            local_file_path (str): the source file to upload
+
+            dropbox_file_path (str): the destination file path 
+
+            access_token (str): access token to connect to Dropbox account
+    """
+    # Authenticate with Dropbox
+    dropbox_connection = dropbox.Dropbox(access_token)
+
+    # Upload the file
+    with open(local_file_path, 'rb') as f:
+        try:
+            dropbox_connection.files_upload(f.read(), dropbox_file_path)
+            print(f'SUCCESS: File "{local_file_path}" uploaded to Dropbox as "{dropbox_file_path}".')
+        except dropbox.exceptions.ApiError as e:
+            print(f'Error uploading file to Dropbox: {e}')
+
+
+if __name__ == '__main__':
+    main()
